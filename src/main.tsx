ca710d31286b71b60bbadf7aa97e5db2ab1c08b8
Michel-Macias <michelmacias.it@gmail.com>
2025-12-03 12:14:00 +0100

    feat: implement PWA functionality by adding Workbox service worker and removing old PWA assets.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 import AppInitializer from "./components/AppInitializer";
-import "./registerServiceWorker";
+
 import "./pushNotifications";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
